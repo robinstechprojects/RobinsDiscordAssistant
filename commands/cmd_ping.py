2f51aa0f9f56aca2c0249db2f7c11c9f8855cce0
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-06-05 17:37:03 +0200

    firstcommit

--- commands/cmd_ping.py
+++ commands/cmd_ping.py
@@ -3,4 +3,4 @@ def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n*Attatched arguments %s*" % args.__str__()[1:-1].replace("'", "")
-    yield from client.send_message(message.channel, "Pong!" + args_out)
+    yield from client.send_message(message.author, "Pong!" + args_out)
