d8db7fdb3dc7e47e0685407831d2d0b58c8a1475
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-06-09 11:12:50 +0200

    radnom pick for wisdoms

--- commands/cmd_wisdom.py
+++ commands/cmd_wisdom.py
@@ -1,5 +1,12 @@
+import random
+
+
 def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n*Attatched arguments %s*" % args.__str__()[1:-1].replace("'", "")
-    yield from client.send_message(message.author, "A few steps Backwards will keep you moving forward" + args_out)
+        datei = open('wisdoms.txt', 'r')
+        sammlung = datei.readlines()
+        datei.close()
+        zitat = random.choice(sammlung)
+    yield from client.send_message(message.author, zitat + args_out)
