a4cce1c3b9c08f34b771c7ba191f57c49323474e
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-06-11 17:10:40 +0200

    dice message in channel
    The bot now posts the dice solution to the channel the command was sent to

--- commands/cmd_dice.py
+++ commands/cmd_dice.py
@@ -8,4 +8,4 @@ def ex(args, message, client, invoke):
         args_out = "\n\n*Attatched arguments %s*" % args.__str__()[1:-1].replace("'", "")
     psolution = randint(1, 6)
     solution = str(psolution)
-    yield from client.send_message(message.author, solution + args_out)
+    yield from client.send_message(message.channel, solution + args_out)
