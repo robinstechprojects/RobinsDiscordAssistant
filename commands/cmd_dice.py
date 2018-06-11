dcd32160ed52c96badfd040ecb68c6e1c1e1e856
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-06-11 17:08:40 +0200

    bugfix#2

--- commands/cmd_dice.py
+++ commands/cmd_dice.py
@@ -6,6 +6,6 @@ def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n*Attatched arguments %s*" % args.__str__()[1:-1].replace("'", "")
-    solution = randint(1, 6)
-    str(solution)
+    psolution = randint(1, 6)
+    solution = str(psolution)
     yield from client.send_message(message.author, solution + args_out)
