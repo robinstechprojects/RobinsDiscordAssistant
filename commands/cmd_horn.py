4458d2098238ccca2eb6329e952939bbae612f24
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-17 11:15:17 +0200

    attempt #3

--- commands/cmd_horn.py
+++ commands/cmd_horn.py
@@ -8,7 +8,7 @@ def ex(args, message, client, invoke):
             player.start()
         except Exception as exc:
             yield from client.send_message(message.channel, "Es ist ein Fehler aufgetreten. ```{ttt}```".format(ttt=exc))
-        voice_client = client.voice_client_in(message.server)
-        yield from voice_client.disconnect()
+
+
 
 
