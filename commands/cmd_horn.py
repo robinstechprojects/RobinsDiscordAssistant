b489815d4faebb58051c807326c3de0f8e006b2e
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-17 11:26:17 +0200

    and again

--- commands/cmd_horn.py
+++ commands/cmd_horn.py
@@ -4,7 +4,7 @@ def ex(args, message, client, invoke):
         try:
             channel = message.author.voice.voice_channel
             voice = yield from client.join_voice_channel(channel)
-            player = voice.create_ffmpeg_player('Sounds\h.mp3')
+            player = voice.create_ffmpeg_player('h.mp3')
             player.start()
         except Exception as exc:
             yield from client.send_message(message.channel, "Es ist ein Fehler aufgetreten. ```{ttt}```".format(ttt=exc))
