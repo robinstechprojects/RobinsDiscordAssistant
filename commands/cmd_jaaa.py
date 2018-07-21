47a8aeb407cf762e1dfa73a82ea9b9c718668093
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-21 21:24:01 +0200

    jaaaa message

--- commands/cmd_jaaa.py
+++ commands/cmd_jaaa.py
@@ -5,12 +5,12 @@ def ex(args, message, client, invoke):
         try:
             channel = message.author.voice.voice_channel
             voice = yield from client.join_voice_channel(channel)
-            player = voice.create_ffmpeg_player('Jaaa.mp3')
+            player = voice.create_ffmpeg_player('Jaaas.mp3')
             player.start()
         except Exception as exc:
             yield from client.send_message(message.channel, "Es ist ein Fehler aufgetreten. ```{ttt}```".format(ttt=exc))
 
         time.sleep(6)
-        yield from client.send_message(message.channel, "MÖP MÖP")
+        yield from client.send_message(message.channel, "JAAA!!!!")
         voice_client = client.voice_client_in(message.server)
         yield from voice_client.disconnect()
