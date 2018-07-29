8f00213a79e9fc051914a4acbf134da10b1ddbd9
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-29 18:33:51 +0200

    integrate metal song

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord import Embed
 import SECRETS
 import asyncio as asyncio
 
-from commands import cmd_ping, STATICS, cmd_wisdom, cmd_dice, cmd_horn, cmd_gamingsong, cmd_chartsong, cmd_moep, cmd_jaaa, cmd_rapsong, cmd_nightcore
+from commands import cmd_ping, STATICS, cmd_wisdom, cmd_dice, cmd_horn, cmd_gamingsong, cmd_chartsong, cmd_moep, cmd_jaaa, cmd_rapsong, cmd_nightcore, cmd_metalsong
 
 print("RDA - DiscordBotSoftware by SunRobinDev started.")
 
@@ -21,7 +21,8 @@ commands = {
     "moep": cmd_moep,
     "jaaa": cmd_jaaa,
     "rapsong": cmd_rapsong,
-    "nightcore": cmd_nightcore
+    "nightcore": cmd_nightcore,
+    "metalsong": cmd_metalsong
 
 }
 
