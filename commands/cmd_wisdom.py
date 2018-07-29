f3cc8dc1dcc89f1b6e01a380bfaddc8da7d807dd
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-29 22:36:14 +0200

    move wisdoms to CDS

--- commands/cmd_wisdom.py
+++ commands/cmd_wisdom.py
@@ -6,8 +6,9 @@ def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n*Attatched arguments %s*" % args.__str__()[1:-1].replace("'", "")
-    file = open('wisdoms.txt', 'r')
-    wisdoms = file.readlines()
-    file.close()
-    qoute = random.choice(wisdoms)
-    yield from client.send_message(message.channel, qoute + args_out)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_addr = ('185.249.199.220', 1338)
+    client_socket.connect(server_addr)
+    client_socket.send(bytes("wisdom", "utf8"))
+    song = client_socket.recv(1024)
+    yield from client.send_message(message.channel,str(song, "utf8") + args_out)
