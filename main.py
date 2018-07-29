b6335370103dfc763b44bca62aedcd8eef807b65
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-29 21:33:57 +0200

    ~rocksong

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord import Embed
 import SECRETS
 import asyncio as asyncio
 
-from commands import cmd_ping, STATICS, cmd_wisdom, cmd_dice, cmd_horn, cmd_gamingsong, cmd_chartsong, cmd_moep, cmd_jaaa, cmd_rapsong, cmd_nightcore, cmd_metalsong
+from commands import cmd_ping, STATICS, cmd_wisdom, cmd_dice, cmd_horn, cmd_gamingsong, cmd_chartsong, cmd_moep, cmd_jaaa, cmd_rapsong, cmd_nightcore, cmd_metalsong, cmd_rocksong
 
 print("RDA - DiscordBotSoftware by SunRobinDev started.")
 
@@ -22,7 +22,8 @@ commands = {
     "jaaa": cmd_jaaa,
     "rapsong": cmd_rapsong,
     "nightcore": cmd_nightcore,
-    "metalsong": cmd_metalsong
+    "metalsong": cmd_metalsong,
+    "rocksong": cmd_rocksong
 
 }
 
