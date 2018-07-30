81915c4725d7aec40cbe76535260bde78ddced41
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-30 19:13:37 +0200

    convert args to string

--- commands/cmd_twitterflw.py
+++ commands/cmd_twitterflw.py
@@ -6,12 +6,13 @@ def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n*Attatched arguments %s*" % args.__str__()[1:-1].replace("'", "")
+        argstr = str(args)
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create InternetSocket for ipV4
 #SOCK_STREAM - TCP SOCK_DGRAM - UDP
         server_addr = ('185.249.199.220', 1338)
         client_socket.connect(server_addr) #connect the socket | UDP does not need connect
         client_socket.send(bytes("tweetrequest", "utf8")) #send message (byte stream)
-        client_socket.send(bytes(args, "utf8")) #send message (byte stream)
+        client_socket.send(bytes(argstr, "utf8")) #send message (byte stream)
         song = client_socket.recv(1024)
         print(str(song, "utf8"))
         client_socket.close()
