72ff7f4038556101d053cd77e67a13eb423e62b0
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-30 19:25:27 +0200

    cutdown args

--- commands/cmd_twitterflw.py
+++ commands/cmd_twitterflw.py
@@ -6,7 +6,7 @@ def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n*Attatched arguments %s*" % args.__str__()[1:-1].replace("'", "")
-        argstr = args.__str__()[1:-1].replace("'", " ")
+        argstr = args.__str__()[1:-1].replace("'", "")
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create InternetSocket for ipV4
 #SOCK_STREAM - TCP SOCK_DGRAM - UDP
         server_addr = ('185.249.199.220', 1338)
