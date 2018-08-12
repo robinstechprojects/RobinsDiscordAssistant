da468c7fcbf3747506019bb89529059cf569f72c
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-08-12 12:09:15 +0200

    make all commands use langfile

--- commands/cmd_horn.py
+++ commands/cmd_horn.py
@@ -1,4 +1,5 @@
 import time
+import langfile
 
 
 def ex(args, message, client, invoke):
@@ -8,9 +9,9 @@ def ex(args, message, client, invoke):
             player = voice.create_ffmpeg_player('h.mp3')
             player.start()
         except Exception as exc:
-            yield from client.send_message(message.channel, "Es ist ein Fehler aufgetreten. ```{ttt}```".format(ttt=exc))
+            yield from client.send_message(message.channel, langfile.lang_hornerror + "```{ttt}```".format(ttt=exc))
 
         time.sleep(2)
-        yield from client.send_message(message.channel, "I just horned !")
+        yield from client.send_message(message.channel, langfile.lang_horn)
         voice_client = client.voice_client_in(message.server)
         yield from voice_client.disconnect()
